cf5be43b30c73b9782610fa212641344f093245c
Edwin Kyalangalilwa <edwinakatosh@icloud.com>
2022-04-02 21:05:17 -0500

    feat: app

--- app/app.go
+++ app/app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	_ "github.com/edwinakatosh/beego-security-headers/app/routers"
+
 	"github.com/beego/beego/v2/server/web"
 	securityHeaders "github.com/edwinakatosh/beego-security-headers"
 )
